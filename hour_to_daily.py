e37c5116b6417c7116b5dc45092e82476f43c745
zheng <zzz0069@auburn.edu>
2018-10-16 18:10:23 -0500

    hour_to_daily_one_day function modified:max_temp and min_temp added

--- hour_to_daily.py
+++ hour_to_daily.py
@@ -14,10 +14,30 @@ from netCDF4 import Dataset
 PATH = '/Users/yymjzq/hydro1.gesdisc.eosdis.nasa.gov/data/NLDAS/NLDAS_FORA0125_H.002/2012/007/'
 lat_110 = 224
 lon_110 = 464
-variable_names = ['PRES_110_SFC', 'TMP_110_HTGL', 'U_GRD_110_HTGL', 
-				  'V_GRD_110_HTGL', 'SPF_H_110_HTGL', 'A_PCP_110_SFC_acc1h', 
-				  'NCRAIN_110_SFC_acc1h', 'CAPE_110_SPDY', 'DSWRF_110_SFC', 
-				  'DLWRF_110_SFC', 'PEVAP_110_SFC_acc1h',]
+HOURS = 24
+
+#variables in original grb file of NLDAS-2 monitor data
+variable_names = ['PRES_110_SFC', 			#Pressure
+				  'TMP_110_HTGL', 			#Temperature
+				  'U_GRD_110_HTGL',			#u-component of wind 
+				  'V_GRD_110_HTGL',			#v-component of wind 
+				  'SPF_H_110_HTGL', 		#Specific humidity
+				  'A_PCP_110_SFC_acc1h', 	#Total precipitation
+				  'NCRAIN_110_SFC_acc1h',	#Number concentration for rain particles 
+				  'CAPE_110_SPDY', 			#Convective available potential energy
+				  'DSWRF_110_SFC', 			#Downward shortwave radiation flux
+				  'DLWRF_110_SFC', 			#Downward longwave radiation flux
+				  'PEVAP_110_SFC_acc1h',	#Potential evaporation
+				  'lat_110',				#latitude
+				  'lon_110']				#longitude	
+
+ignore_varnames = ['NCRAIN_110_SFC_acc1h',	
+				  'CAPE_110_SPDY', 			
+				  'DSWRF_110_SFC', 			
+				  'DLWRF_110_SFC', 			
+				  'PEVAP_110_SFC_acc1h',
+				  'lat_110',
+				   'lon_110']
 
 def grb_file_name_one_day(file_path):
 	file_name_list = []
@@ -33,17 +53,29 @@ def hour_to_daily_one_day():
 	for grb in grbs:
 		nios = Nio.open_file(grb, mode='r', options=None, history='', format='')
 		varNames = nios.variables.keys()
-		ignore_varnames = ['lat_110', 'lon_110']
 		if grb_one_day == {}:
 			for varName in varNames:
 				if varName in ignore_varnames:
 					continue
-				grb_one_day['%s'%varName] = nios.variables[varName].get_value()
+				if varName == 'TMP_110_HTGL':
+					grb_one_day['MAX_%s'%varName] = nios.variables[varName].get_value()	
+					grb_one_day['MIN_%s'%varName] = nios.variables[varName].get_value()
+				else:
+					grb_one_day['%s'%varName] = nios.variables[varName].get_value()
 		else:
 			for varName in varNames:
 				if varName in ignore_varnames:
 					continue
-				grb_one_day['%s'%varName] += nios.variables[varName].get_value()
+				if varName == 'TMP_110_HTGL':
+					grb_one_day['MAX_%s'%varName] = np.maximum(nios.variables[varName].get_value(), grb_one_day['MAX_%s'%varName])	
+					grb_one_day['MIN_%s'%varName] = np.minimum(nios.variables[varName].get_value(), grb_one_day['MIN_%s'%varName])
+				else:
+					grb_one_day['%s'%varName] += nios.variables[varName].get_value()
+	for key, value in grb_one_day.items():
+		if key in ['A_PCP_110_SFC_acc1h', 'MAX_TMP_110_HTGL', 'MAX_TMP_110_HTGL']:
+			continue
+		else:
+			grb_one_day[key] = value / HOURS
 	return grb_one_day
 
 def create_empty_netCDF():
