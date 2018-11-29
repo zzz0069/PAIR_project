18e12dc971f05156ef1e7a2539d25f75c440f378
bwv0004@tigermail.auburn.edu <bwv0004@tigermail.auburn.edu>
2018-11-28 22:00:48 -0600

    create functions for consecutive hot/cold days

--- query_NLDAS.py
+++ query_NLDAS.py
@@ -7,7 +7,10 @@ import numpy as np
 import glob
 import math
 from netCDF4 import Dataset
+from pyeto import convert
 
+hotTemp = 20 #degrees celcius
+coldTemp = 0 #degrees celcius
 
 path = '/netCDF/'
 
@@ -180,12 +183,122 @@ def queryFileAggregateDateRangeSingleCoordinate(querystartdate, queryenddate, va
     return aggregate
 
 
+
+#this function will return the consecutive hot or cold days a date range for a
+#single coordinate represented by  lat/lon
+#aggregatefunction can be hot or cold
+#returns a single value
+def queryFileConsecutiveHotColdDateRangeSingleCoordinate(querystartdate, queryenddate, variable, aggregatefunction, lat, lon):
+    file_name_list = getfilenames(querystartdate, queryenddate)
+
+    aggregate = 0
+
+    filecount = 0
+    for filename in file_name_list:
+        filecount += 1
+        ds = Dataset(filename)
+
+        #grab the lat and lon variable arrays
+        lats = ds.variables['lat_110'][:]
+        lons = ds.variables['lon_110'][:]
+
+        # calculate the lower and upper indicies of the lat array
+        latindex = np.nonzero(lats == lat)[0][0]
+
+        # calculate the lower and upper indices of the lon array
+        lonindex = np.nonzero(lons == lon)[0][0]
+
+        # grab the dataset for the given variable name and rectangle
+        dataSubset = ds.variables[variable][latindex, lonindex]
+        dataSubset = convert.kelvin2celsius(dataSubset)
+
+        if aggregatefunction == "hot":
+            if dataSubset>=hotTemp:
+                aggregate += 1
+            else:
+                aggregate = 0
+
+        elif aggregatefunction == "cold":
+            if dataSubset<=coldTemp:
+                aggregate += 1
+            else:
+                aggregate = 0
+
+
+    return aggregate
+
+#this function will return the consecutive hot or cold days a date range for a
+#rectangular area represented by upper and lower lat/lon
+#lat_bounds and lon_boundsshould be an array with 2 variables in radians [lower value, upper value]
+#aggregatefunction can be hot or cold
+#returns a 2d array
+def queryFileConsecutiveHotColdDateRangeRectangle(querystartdate, queryenddate, variable, aggregatefunction, lat_bounds, lon_bounds):
+    file_name_list = getfilenames(querystartdate, queryenddate)
+
+    aggregate = []
+    firstLoop = 0
+
+    filecount = 0
+    for filename in file_name_list:
+        filecount += 1
+        ds = Dataset(filename)
+
+        #grab the lat and lon variable arrays
+        lats = ds.variables['lat_110'][:]
+        lons = ds.variables['lon_110'][:]
+
+        #calculate the lower and upper indicies of the lat array
+        latlowerindex = np.argmin(np.abs(lats - lat_bounds[0]))
+        latupperindex = np.argmin(np.abs(lats - lat_bounds[1]))
+
+        #calculate the lower and upper indices of the lon array
+        lonlowerindex = np.argmin(np.abs(lons - lon_bounds[0]))
+        lonupperindex = np.argmin(np.abs(lons - lon_bounds[1]))
+
+        #grab the dataset for the given variable name and rectangle
+        dataSubset = ds.variables[variable][latlowerindex:latupperindex, lonlowerindex:lonupperindex]
+
+        #initialize return aggregate
+        if firstLoop == 0:
+            firstLoop = 1
+            rows = dataSubset.shape[0]  # calculate number of rows in array
+            cols = dataSubset.shape[1]  # calculate number of columns in array
+            aggregate = np.zeros((rows,cols))
+
+        rows = aggregate.shape[0]  # calculate number of rows in array
+        cols = aggregate.shape[1]  # calculate number of columns in array
+        for i in range(0, rows):
+            for j in range(0, cols):
+
+                dataSubset[i, j] = convert.kelvin2celsius(dataSubset[i, j])
+                if aggregatefunction == "hot":
+                    if dataSubset[i, j] >= hotTemp:
+                        aggregate[i, j] += 1
+                    else:
+                        aggregate[i, j] = 0
+
+                elif aggregatefunction == "cold":
+                    if dataSubset[i, j] <= coldTemp:
+                        aggregate[i, j] += 1
+                    else:
+                        aggregate[i, j] = 0
+
+
+    return aggregate
+
+
 lat_bnds = [25.0, 26.0]
 lon_bnds = [-108, -107]
 
 #print(queryFileSingleDateRectangle(date(2018, 1, 1), 'MAX_TMP_110_HTGL', lat_bnds, lon_bnds))
 #print(queryFileSingleDateSingleCoordinate(date(2018, 1, 1), 'MAX_TMP_110_HTGL', 25.063, -107.938))
 
-print(queryFileAggregateDateRangeRectangle(date(2018, 1, 1), date(2018, 1, 3), 'MAX_TMP_110_HTGL', 'avg', lat_bnds, lon_bnds))
+#print(queryFileAggregateDateRangeRectangle(date(2018, 1, 1), date(2018, 1, 3), 'AVG_MAX_MIN_TMP_110_HTGL', 'avg', lat_bnds, lon_bnds))
 
-print(queryFileAggregateDateRangeSingleCoordinate(date(2018, 1, 1), date(2018, 1, 3), 'MAX_TMP_110_HTGL', 'min', 25.063, -107.938))
+#print(queryFileAggregateDateRangeSingleCoordinate(date(2018, 1, 1), date(2018, 1, 3), 'MAX_TMP_110_HTGL', 'min', 25.063, -107.938))
+
+
+#print(queryFileConsecutiveHotColdDateRangeSingleCoordinate(date(2018, 1, 1), date(2018, 1, 3), 'AVG_MAX_MIN_TMP_110_HTGL', 'cold', 25.063, -107.938))
+
+
+print(queryFileConsecutiveHotColdDateRangeRectangle(date(2018, 1, 1), date(2018, 1, 3), 'AVG_MAX_MIN_TMP_110_HTGL', 'hot', lat_bnds, lon_bnds))
